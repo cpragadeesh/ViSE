58b80c316e019412779bcc4511a07f580e5db30f
cpragadeesh <cpragadeesh@gmail.com>
2015-06-27 18:18:30 +0530

    Imgur Intergration
    imager upload script

---
+++ python/upload.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+'''
+	Here's how you upload an image. For this example, put the cutest picture
+	of a kitten you can find in this script's folder and name it 'Kitten.jpg'
+
+	For more details about images and the API see here:
+		https://api.imgur.com/endpoints/image
+'''
+
+# Pull authentication from the auth example (see auth.py)
+from auth import authenticate
+from datetime import datetime
+import os
+
+album = None # You can also enter an album ID here
+image_path = 'images/'
+
+def upload_kitten(client):
+	'''
+		Upload a picture of a kitten. We don't ship one, so get creative!
+	'''
+
+	# Here's the metadata for the upload. All of these are optional, including
+	# this config dict itself.
+	config = {
+		'album': album,
+		'name':  'Catastrophe!',
+		'title': 'Catastrophe!',
+		'description': 'Cute kitten being cute on {0}'.format(datetime.now())
+	}
+
+        urllist = []
+	print("Uploading image... ")
+	#image = client.upload_from_path(image_path, config=config, anon=False)
+        for img in os.listdir(image_path):
+                if img.endswith('jpg'):
+                        print "Uploading path " + image_path + img 
+                        image = client.upload_from_path(image_path + img, config=config, anon=False)
+                        urllist.append(image['link'])
+        
+	return urllist
+
+
+# If you want to run this as a standalone script
+if __name__ == "__main__":
+	client = authenticate()
+	urllist = upload_kitten(client)
+
+	print("Image was posted! Go check your images you sexy beast!")
+        print urllist
